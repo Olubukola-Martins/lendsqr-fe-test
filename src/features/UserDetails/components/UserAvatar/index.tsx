c0dee5d57627b719f2d164d25b443c456f1d4ca1
Olubukola-Martins <19bukola97@gmail.com>
2025-08-24 17:37:49 +0100

    fix: UI style changes

--- src/features/UserDetails/components/UserAvatar/index.tsx
+++ src/features/UserDetails/components/UserAvatar/index.tsx
@@ -7,19 +7,23 @@ type Props = {
   tier: number;
   balance: string;
   accountNumber: string;
-  userCode:string;
+  userCode: string;
   imageUrl?: string;
   altText?: string;
 };
 
 export const UserAvatar: React.FC<Props> = ({
   name,
-  tier,
+  // tier,
   balance,
   accountNumber,
   altText,
-  imageUrl,userCode
+  imageUrl,
+  userCode,
 }) => {
+  const stars = [1, 2, 3];
+  const rating = 1;
+
   return (
     <div className="user-avatar">
       <div className="user-avatar__info">
@@ -39,8 +43,21 @@ export const UserAvatar: React.FC<Props> = ({
           <div className="user-avatar__name">{name}</div>
           <div className="user-avatar__code">{userCode}</div>
         </div>
-          <div className="user-avatar__tier">User’s Tier <span>⭐{tier}</span></div>
-        
+        <div className="user-avatar__tier">
+          User’s Tier
+          {/* <span>⭐{tier}</span> */}
+          <div className="star-rating">
+            {stars.map((star) => (
+              <svg
+                key={star}
+                className={`star ${star <= rating ? "filled" : "outlined"}`}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24">
+                <path d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l2.286 7.03h7.386c.969 0 1.371 1.24.588 1.81l-5.974 4.342 2.286 7.03c.3.921-.755 1.688-1.538 1.118L12 18.347l-5.973 4.342c-.783.57-1.838-.197-1.538-1.118l2.286-7.03-5.974-4.342c-.783-.57-.381-1.81.588-1.81h7.386l2.286-7.03z" />
+              </svg>
+            ))}
+          </div>
+        </div>
       </div>
       <div className="user-avatar__account">
         <div className="user-avatar__balance">₦{balance}</div>
